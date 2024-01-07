e3d6a34f26cfeb3a913a6f86e57fdd6215898d01
Tulitanir <ne4aev.denis2010@yandex.ru>
2024-01-08 00:33:06 +0400

    Change docker-compose.yml

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 git submodule update --init --recursive --remote
-docker compose up -d && echo Интерфейс должен быть доступен по URL: http://localhost:3000
+docker compose up -d --build && echo Интерфейс должен быть доступен по URL: http://localhost:3000
 pause
